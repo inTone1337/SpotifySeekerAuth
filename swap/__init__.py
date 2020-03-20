9a0a9f5c01588a4777ea900e25f144a30337a557
inTone1337 <59336850+inTone1337@users.noreply.github.com>
2020-03-20 10:13:40 +0100

    Added expected JSON request for /swap endpoint.

--- swap/__init__.py
+++ swap/__init__.py
@@ -4,22 +4,14 @@ import azure.functions as func
 
 spotify_token_endpoint = "https://accounts.spotify.com/api/token"
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    json = req.get_json()
+    grant_type = json["grant_type"]
+    code = json["code"]
+    redirect_uri = json["redirect_uri"]
+    return func.HttpResponse(f"Received {json}.")
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+# TODO: Add separate routing and logic for /swap and /refresh endpoints.
+def refresh(req: func.HttpRequest) -> func.HttpResponse:
+    return None
