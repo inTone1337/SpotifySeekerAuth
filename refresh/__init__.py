cb832ff6927b66bdc8b25dcc1f3b5ddc8e18a326
inTone1337 <59336850+inTone1337@users.noreply.github.com>
2020-03-24 08:52:54 +0100

    - Added expected JSON response for both endpoints. - Deleted sample.dat (unnecessary).

--- refresh/__init__.py
+++ refresh/__init__.py
@@ -1,12 +1,19 @@
 import logging
-
+import json
 import azure.functions as func
 
 spotify_token_endpoint = "https://accounts.spotify.com/api/token"
 
 
 def main(request: func.HttpRequest) -> func.HttpResponse:
-    json = request.get_json()
-    grant_type = json["grant_type"]
-    refresh_token = json["refresh_token"]
-    return func.HttpResponse(f"Received {json}.")
+    request_json = request.get_json()
+    grant_type = request_json["grant_type"]
+    refresh_token = request_json["refresh_token"]
+
+    # TODO: Fill this with data received from Spotify.
+    response = {}
+    response["access_token"] = None
+    response["expires_in"] = None
+    response_json = json.dumps(response)
+
+    return func.HttpResponse(f"Received {request_json}. Replied {response_json}.")
--- swap/__init__.py
+++ swap/__init__.py
@@ -1,13 +1,21 @@
 import logging
-
+import json
 import azure.functions as func
 
 spotify_token_endpoint = "https://accounts.spotify.com/api/token"
 
 
 def main(request: func.HttpRequest) -> func.HttpResponse:
-    json = request.get_json()
-    grant_type = json["grant_type"]
-    code = json["code"]
-    redirect_uri = json["redirect_uri"]
-    return func.HttpResponse(f"Received {json}.")
+    request_json = request.get_json()
+    grant_type = request_json["grant_type"]
+    code = request_json["code"]
+    redirect_uri = request_json["redirect_uri"]
+
+    # TODO: Fill this with data received from Spotify.
+    response = {}
+    response["access_token"] = None
+    response["expires_in"] = None
+    response["refresh_token"] = None
+    response_json = json.dumps(response)
+
+    return func.HttpResponse(f"Received {request_json}. Replied {response_json}.")
