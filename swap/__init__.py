a039e19570c13f2c91c992fa76c7aa3e3ca08732
inTone1337 <59336850+inTone1337@users.noreply.github.com>
2020-03-31 14:21:32 +0200

    - Pass data received from the Spotify token endpoint to Spotify Seeker. - Renamed spotify_secret to spotify_client_secret.

--- swap/__init__.py
+++ swap/__init__.py
@@ -4,20 +4,10 @@ import requests
 import azure.functions as func
 
 spotify_client_id = None
-spotify_secret = None
+spotify_client_secret = None
 spotify_token_endpoint = "https://accounts.spotify.com/api/token"
 
 
 def main(request: func.HttpRequest) -> func.HttpResponse:
-    # TODO: Add Spotify Client ID and Secret to request.
-    spotify_request = requests.post(spotify_token_endpoint, request.get_json())
-    logging.debug(spotify_request.text)
-
-    # TODO: Fill this with data received from Spotify.
-    response = {}
-    response["access_token"] = None
-    response["expires_in"] = None
-    response["refresh_token"] = None
-    response_json = json.dumps(response)
-
-    return func.HttpResponse(f"Received {request.get_json()}. Replied {response_json}.")
+    request_json = request.get_json()
+    return requests.post(spotify_token_endpoint, request_json)
