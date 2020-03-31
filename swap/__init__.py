28961741d66067cd34d4e7a845cd9c3e181ae3af
inTone1337 <59336850+inTone1337@users.noreply.github.com>
2020-03-31 14:29:43 +0200

    Re-added TODO regarding Spotify Client ID and Secret.

--- swap/__init__.py
+++ swap/__init__.py
@@ -10,4 +10,5 @@ spotify_token_endpoint = "https://accounts.spotify.com/api/token"
 
 def main(request: func.HttpRequest) -> func.HttpResponse:
     request_json = request.get_json()
+    # TODO: Add Spotify Client ID and Secret to request.
     return requests.post(spotify_token_endpoint, request_json)
